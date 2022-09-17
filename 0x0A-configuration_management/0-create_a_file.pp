6291d5d5fd1c34babd37696c09a31b46fe2fcdf4
rioles <rioles1992@gmail.com>
2022-09-17 12:41:02 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Create a file in /tmp
-
-file { 'holberton':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
+# Create a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/holberton',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
